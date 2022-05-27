f7c55a630a640441ffaa53d4ea84ef32b3c622d8
shforeal <rivena14798@gmail.com>
2022-05-27 22:19:38 +0300

    'first'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
 
 function App() {
+
+  const canvasRef = useRef(null)
+
+  const draw = () => {
+    const ctx = canvasRef.current.getContext('2d')
+    // Set styling
+    const color = '#111'//Math.floor(Math.random()*16777215).toString(16);
+    ctx.strokeStyle = '#' + color
+    ctx.font = '18px Arial';
+
+    // Draw rectangles and text
+    ctx.beginPath();
+    ctx.fillStyle = '#' + color
+    ctx.fillText('hi', 50, 50);
+    ctx.rect(10, 10, 100, 100);
+    ctx.stroke();
+  }
+
+
+  useEffect(() => { draw() }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Welcome to canvas world</h1>
+        </div>
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            background: 'white',
+            width: 640,
+            height: 480,
+          }}
+        ></canvas>
       </header>
     </div>
   );
